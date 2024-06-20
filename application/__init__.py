46681581e38eae046243b98f0702e2be3df44a34
pablo-marin-vicuna <pmarin@andrew.cmu.edu>
2024-06-20 14:16:22 -0400

    update requirements

--- application/__init__.py
+++ application/__init__.py
@@ -7,6 +7,7 @@ from application.rf_prediction import *
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 
+app = Flask(__name__)
 
 ###############
 ## Data Load
